4547951770d808cfb669a4e99860274c10056eae
alfille <paul.alfille@gmail.com>
2021-05-10 21:55:52 -0400

    Start -- put in alphabet

---
+++ pct.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#
+# pi_cw_trainer
+# http://github.com/alfille/pi_cw_trainer
+# Combined hardware and software project
+# Use a Pi to generate random CW
+# audio output: buzzer
+# screen output on LCD screen
+# potentometer to adjust speed
+#
+# 2021 Paul H Alffille
+
+class code:
+    cw = {
+    'a' : '.-'    ,
+    'b' : '-...'  ,
+    'c' : '-.-.'  ,
+    'd' : '-..'   ,
+    'e' : '.'     ,
+    'f' : '..-.'  ,
+    'g' : '--.'   ,
+    'h' : '....'  ,
+    'i' : '..'    ,
+    'j' : .---'   ,
+    'k' : '-.-'   ,
+    'l' : '.-..'  ,
+    'm' : '--'    ,
+    'n' : '-.'    ,
+    'o' : '---'   ,
+    'p' : '.--.'  ,
+    'q' : '--.-'  ,
+    'r' : '.-.'   ,
+    's' : '...'   ,
+    't' : '-'     ,
+    'u' : '..-'   ,
+    'v' : '...-'  ,
+    'w' : '.--'   ,
+    'x' : '-..-'  ,
+    'y' : '-.--'  ,
+    'z' : '--..'  ,
+    }
+    
+    
